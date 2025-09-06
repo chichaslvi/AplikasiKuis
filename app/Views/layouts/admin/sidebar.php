a544f8d4db65a68cc916709e3e316674be3c9f84
chichaslvi <chichaslvi@gmail.com>
2025-09-06 22:49:31 +0700

    update routes role

--- app/Views/layouts/admin/sidebar.php
+++ app/Views/layouts/admin/sidebar.php
@@ -15,7 +15,7 @@
       </a>
     </li>
     <li>
-      <a href="<?= base_url('admin/roles/index') ?>">
+      <a href="<?= base_url('admin/roles') ?>">
         <i class="fa-solid fa-user-gear"></i> Manajemen Role
       </a>
     </li>
@@ -30,7 +30,7 @@
       </a>
     </li>
     <li>
-      <a href="<?= base_url('logout') ?>">
+      <a href="<?= base_url('auth/logout') ?>">
         <i class="fa-solid fa-right-from-bracket"></i> Logout
       </a>
     </li>
